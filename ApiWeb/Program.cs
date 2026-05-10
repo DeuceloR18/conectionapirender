337ab1ce406be19465a24da09cad2a5588ae4da6
DeuceloR18 <daniloucelo087@gmail.com>
2026-05-09 22:30:49 -0600

    cs modificado

--- ApiWeb/Program.cs
+++ ApiWeb/Program.cs
@@ -3,6 +3,9 @@ var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://*:{port}");
 
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
